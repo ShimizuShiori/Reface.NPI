8ce41b747628ed46595ce37fc5d6c557b59328c4
ShimizuShiori <116971263@qq.com>
2020-03-07 15:52:37 +0800

    修改单元测试名称

--- src/Reface.NPITests/Parsers/DefaultSelectParserTests.cs
+++ src/Reface.NPITests/Parsers/DefaultSelectParserTests.cs
@@ -12,7 +12,7 @@ namespace Reface.NPI.Parsers.Tests
     public class DefaultSelectParserTests
     {
         [TestMethod()]
-        public void SplitCommandToTokensTest()
+        public void SplitCommandToTokensTest3Words()
         {
             string command = "NameById";
             DefaultSelectParser parser = new DefaultSelectParser();
@@ -24,6 +24,18 @@ namespace Reface.NPI.Parsers.Tests
             Assert.AreEqual(SelectParseActions.By, tokens[1].Action);
             Assert.AreEqual("Id", tokens[2].Text);
             Assert.AreEqual(SelectParseActions.Field, tokens[2].Action);
+        }
+
+
+        [TestMethod()]
+        public void SplitCommandToTokensTestSingleWord()
+        {
+            string command = "Name";
+            DefaultSelectParser parser = new DefaultSelectParser();
+            List<SelectToken> tokens = parser.SplitCommandToTokens(command);
+            Assert.AreEqual(1, tokens.Count);
+            Assert.AreEqual("Name", tokens[0].Text);
+            Assert.AreEqual(SelectParseActions.Field, tokens[0].Action);
         }
     }
 }
