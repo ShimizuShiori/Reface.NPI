7316317f81c20fd96a77763486243a3c5463268f
ShimizuShiori <116971263@qq.com>
2020-03-07 16:16:19 +0800

    对查询状态的编码完成，测试未通过，可能存在状态不够细的问题

--- src/Reface.NPITests/Parsers/DefaultSelectParserTests.cs
+++ src/Reface.NPITests/Parsers/DefaultSelectParserTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reface.NPI.Models;
 using Reface.NPI.Parsers;
 using System;
 using System.Collections.Generic;
@@ -36,6 +37,20 @@ namespace Reface.NPI.Parsers.Tests
             Assert.AreEqual(1, tokens.Count);
             Assert.AreEqual("Name", tokens[0].Text);
             Assert.AreEqual(SelectParseActions.Field, tokens[0].Action);
+        }
+
+        [TestMethod()]
+        public void ParseTest_NameById()
+        {
+            string command = "NameById";
+            DefaultSelectParser parser = new DefaultSelectParser();
+            SelectInfo info = parser.Parse(command);
+            Assert.AreEqual(1, info.Fields.Count, "count of field should be 1");
+            Assert.AreEqual("Name", info.Fields[0]);
+            Assert.AreEqual(1, info.Conditions.Count, "count of condition should be 1");
+            Assert.AreEqual("Id", info.Conditions[0].Field);
+            Assert.AreEqual(ConditionOperators.Is, info.Conditions[0].Operators);
+            Assert.AreEqual(0, info.Orders.Count);
         }
     }
 }
