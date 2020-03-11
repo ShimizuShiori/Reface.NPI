0449e5d9992cbeff575c7ada86a68790653f716c
ShimizuShiori <116971263@qq.com>
2020-03-11 22:28:18 +0800

    create type UpdateToken

---
+++ src/Reface.NPI/Parsers/Tokens/UpdateToken.cs
@@ -0,0 +1,34 @@
+﻿using Reface.NPI.Parsers.Actions;
+using System;
+
+namespace Reface.NPI.Parsers.Tokens
+{
+    public class UpdateToken : IToken<UpdateParseActions>
+    {
+        private const string TEXT_AND = "And";
+        private const string TEXT_OR = "Or";
+        private const string TEXT_BY = "By";
+
+
+        public string Text { get; private set; }
+
+        public UpdateParseActions Action { get; private set; }
+
+        private UpdateToken(string text, UpdateParseActions action)
+        {
+            Text = text;
+            Action = action;
+        }
+
+        public static UpdateToken Create(string text)
+        {
+            switch (text)
+            {
+                case TEXT_AND: return new UpdateToken(text, UpdateParseActions.And);
+                case TEXT_OR: return new UpdateToken(text, UpdateParseActions.Or);
+                case TEXT_BY: return new UpdateToken(text, UpdateParseActions.By);
+                default: return new UpdateToken(text, UpdateParseActions.Word);
+            }
+        }
+    }
+}
