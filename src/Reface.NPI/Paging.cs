e33e5021b5db4b62473700b95b4caa4d8690e14f
ShimizuShiori <116971263@qq.com>
2020-03-31 14:09:26 +0800

    添加分页参数

---
+++ src/Reface.NPI/Paging.cs
@@ -0,0 +1,15 @@
+﻿namespace Reface.NPI
+{
+    public class Paging
+    {
+        /// <summary>
+        /// 分页大小，即每页显示数量
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 页数，0表示第一页
+        /// </summary>
+        public int PageIndex { get; set; }
+    }
+}
